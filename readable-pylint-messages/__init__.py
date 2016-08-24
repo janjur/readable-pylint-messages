9188fbf1c4292bbb9f30e5452f712abad0ebc177
Jurec Jan <jan.jurec@nokia.com>
2016-08-24 23:06:46 +0200

    fixed style, lengthened line-size

--- readable-pylint-messages/__init__.py
+++
@@ -1,1 +0,0 @@
-from .generate_markdown import main # pylint: disable=unused-import
--- readable_pylint_messages/generate_markdown.py
+++ readable_pylint_messages/generate_markdown.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """ generate_markdown docstring
 This module is complete tool for creating as eyepleasing as possible pylint messages
 with error codes and descriptions.
