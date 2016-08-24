6057a3066764562808ce496aab235ef266d86e2e
Jurec Jan <jan.jurec@nokia.com>
2016-08-24 22:49:56 +0200

    tweaked generator, CI enabled

--- generate_markdown.py
+++ generate_markdown.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+from subprocess import check_output, PIPE
 
 
+def main():
+    pylint_messages = check_output(['pylint', '--list-msgs'], stderr=PIPE)
+    print(pylint_messages.decode('utf-8'))
 
-
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
