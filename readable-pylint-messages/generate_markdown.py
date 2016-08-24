b4c7ba7ff3d2ffeeee3071086dc937df266859e1
Jurec Jan <jan.jurec@nokia.com>
2016-08-24 23:00:18 +0200

    modularized, fixed pylint errors

---
+++ readable-pylint-messages/generate_markdown.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" generate_markdown docstring
+This module is complete tool for creating as eyepleasing as possible pylint messages
+with error codes and descriptions.
+"""
+
+from subprocess import check_output, PIPE
+
+
+def main():
+    """
+    Function handles generating whole README.md
+
+    :return: exit code
+    """
+    pylint_messages = check_output(['pylint', '--list-msgs'], stderr=PIPE)
+    print(pylint_messages.decode('utf-8'))
+
+if __name__ == '__main__':
+    main()
