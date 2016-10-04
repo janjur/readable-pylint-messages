08be8170d06c97faeb924e076fc46de622f47d3d
Jan Jurec <jan.jurec@nokia.com>
2016-10-04 22:05:50 +0200

    generating pylint msgs

--- generate_markdown.py
+++ generate_markdown.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+from subprocess import check_output, PIPE
 
+def pylint_list_msgs():
+	return check_output(['pylint', '--list-msgs'], stderr=PIPE).decode('utf-8')
 
+def main():
+    print(pylint_list_msgs())
 
-
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
