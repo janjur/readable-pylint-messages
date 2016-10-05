6595df26c39f53132859b86da3bb1077641d6768
Jan Jurec <jan.jurec@nokia.com>
2016-10-05 12:49:09 +0200

    reverted changes. God, help me...

--- generate_markdown.py
+++ generate_markdown.py
@@ -3,7 +3,7 @@ from subprocess import check_output, PIPE
 from re import compile, findall
 
 def pylint_list_msgs():
-	return check_output(['pylint', '--list-msgs'], stderr=PIPE).decode('utf-8')
+    return check_output(['pylint', '--list-msgs'], stderr=PIPE).decode('utf-8')
 
 def prepare_list_of_matches(pattern, text):
     comp = compile(pattern)
@@ -13,10 +13,11 @@ def prepare_list_of_matches(pattern, text):
         new_matches.append((match[0], match[1], match[2], match[3].strip()))
     return new_matches
 
+
 def main():
     msgs = pylint_list_msgs()
     list_of_msgs = prepare_list_of_matches(r":([\w-]+) \((\w+)\): \*([\w \"%]+)\*\n([\w ,\(\)'\n.]+)", msgs)
-    
+    print(list_of_msgs)
 
 if __name__ == '__main__':
     main()
