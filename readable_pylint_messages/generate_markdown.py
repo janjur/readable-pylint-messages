201e7d176d553701965a36bd2d0609d9d8b0ff88
Jan Jurec <jan.jurec@nokia.com>
2016-10-05 18:59:45 +0200

    Finally merged main file

--- readable_pylint_messages/generate_markdown.py
+++ readable_pylint_messages/generate_markdown.py
@@ -3,8 +3,21 @@
 This module is complete tool for creating as eyepleasing as possible pylint messages
 with error codes and descriptions.
 """
-
 from subprocess import check_output, PIPE
+from re import compile, findall
+
+
+def pylint_list_msgs():
+    return check_output(['pylint', '--list-msgs'], stderr=PIPE).decode('utf-8')
+
+
+def prepare_list_of_matches(pattern, text):
+    comp = compile(pattern)
+    matches = findall(comp, text)
+    new_matches = []
+    for match in matches:
+        new_matches.append((match[0], match[1], match[2], match[3].strip()))
+    return new_matches
 
 
 def main():
@@ -13,8 +26,9 @@ def main():
 
     :return: exit code
     """
-    pylint_messages = check_output(['pylint', '--list-msgs'], stderr=PIPE)
-    print(pylint_messages.decode('utf-8'))
+    msgs = pylint_list_msgs()
+    list_of_msgs = prepare_list_of_matches(r":([\w-]+) \((\w+)\): \*([\w \"%]+)\*\n([\w ,\(\)'\n.]+)", msgs)
+    print(list_of_msgs)
 
 if __name__ == '__main__':
     main()
