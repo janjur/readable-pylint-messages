f83f0264fbeaa493dd7f9dab537d734dafd6e746
Jan Jurec <jan.jurec@nokia.com>
2016-10-05 19:09:51 +0200

    adjusted for pylint compability

--- readable_pylint_messages/generate_markdown.py
+++ readable_pylint_messages/generate_markdown.py
@@ -4,16 +4,28 @@ This module is complete tool for creating as eyepleasing as possible pylint mess
 with error codes and descriptions.
 """
 from subprocess import check_output, PIPE
-from re import compile, findall
+import re
 
 
 def pylint_list_msgs():
+    """
+    Get every pylint message
+
+    :return: string
+    """
     return check_output(['pylint', '--list-msgs'], stderr=PIPE).decode('utf-8')
 
 
 def prepare_list_of_matches(pattern, text):
-    comp = compile(pattern)
-    matches = findall(comp, text)
+    """
+    Convert string from pylint to tuples
+    
+    :param pattern: regex pattern to compile
+    :param text: text to check for msgs
+    :return: list of formatted tuples each being msg
+    """
+    comp = re.compile(pattern)
+    matches = re.findall(comp, text)
     new_matches = []
     for match in matches:
         new_matches.append((match[0], match[1], match[2], match[3].strip()))
